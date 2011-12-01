841165906349d7c730ad6d14d90bd3d7625be1a4
Nathan Ostlie <nostlie@gmail.com>
2011-12-01 15:04:40 -0600

    added LINQ to SQL implementation

--- MvcWithAutomapper/MvcWithAutomapper/Controllers/HomeController.cs
+++ MvcWithAutomapper/MvcWithAutomapper/Controllers/HomeController.cs
@@ -6,58 +6,82 @@ using System.Web.Mvc;
 using AutoMapper;
 using System.Data.SqlClient;
 using System.Data;
+using ADO = MvcWithAutomapper.Models.ADO;
+using LinqToSql = MvcWithAutomapper.Models.LinqToSql;
 
 namespace MvcWithAutomapper.Controllers
 {
     public class HomeController : Controller
     {
+        //// ADO.NET Entity Framework classes
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Message = "Blogs or something:";
+        //    var db = new ADO.TestDBEntities();
+        //    var blogs = db.Blogs.ToArray();
+        //    Mapper.CreateMap<ADO.Blog, Models.BlogSummary>()
+        //        .AfterMap((src, dest) => dest.AuthorBio += "$")
+        //        .ForMember(x => x.AuthorBio, opt =>
+        //                                        {
+        //                                            opt.NullSubstitute("N/A");
+        //                                            opt.AddFormatter<BlogFormatter>();
+        //                                        });
+
+        //    var summaries = Mapper.Map<ADO.Blog[], Models.BlogSummary[]>(blogs);
+
+        //    return View(summaries.ToArray());
+        //}
+
+        //// Used SqlConnections
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Message = "Blogs or something:";
+
+        //    Mapper.Reset();
+        //    Mapper.CreateMap<IDataReader, Models.BlogSummary>()
+        //        .ForMember(m => m.AuthorName, opt => opt.MapFrom(r => r.GetString(r.GetOrdinal("Name"))))
+        //        .ForMember(m => m.AuthorAge, opt => opt.MapFrom(r => r.GetInt32(r.GetOrdinal("Age"))))
+        //        .ForMember(m => m.AuthorBio, opt => opt.MapFrom(r => r.GetString(r.GetOrdinal("Bio"))));
+
+        //    IEnumerable<Models.BlogSummary> summaries = null;
+
+        //    using (SqlConnection connection = new SqlConnection(@"server=.\SQLEXPRESS; DataBase=TestDB; Integrated Security=SSPI"))
+        //    {
+        //        var command = connection.CreateCommand();
+        //        command.CommandText = "dbo.GetAllBlogs";
+        //        command.CommandType = System.Data.CommandType.StoredProcedure;
+        //        connection.Open();
+        //        summaries = Mapper.Map<IDataReader, IEnumerable<Models.BlogSummary>>(command.ExecuteReader());
+        //    }
+
+
+        //    return View(summaries.ToArray());
+        //}
+
+        // Used LINQ to SQL implementation
         public ActionResult Index()
         {
-            ViewBag.Message = "Blogs or something:";
-
-            //var blogs = new Models.BlogModel[]
-            //{
-            //    new Models.BlogModel { Author = new Author { Age = 55, Bio = "Neat guy", HomeTown = "Minneapolis", Name = "Jerry Iatrics" }, Description = "Blog about health topics for the elderly.", Title = "Growing old" },
-            //    new Models.BlogModel { Author = new Author { Age = 35, Bio = "Fun guy", HomeTown = "St. Paul", Name = "Poe Diatry" }, Description = "Blog about avoiding injuries to your feet.", Title = "Happy Feet" },
-            //    new Models.BlogModel { Author = new Author { Age = 22, HomeTown = "Blaine", Name = "Lyona Round" }, Description = "Blog about keeping sedentary.", Title = "Guide to a Lazy Life" }
-            //};
-
-            //var db = new Models.TestDBEntities();
-            //var blogs = db.Blogs.ToArray();          
+            LinqToSql.TestDBLinqToSqlDataContext db = new LinqToSql.TestDBLinqToSqlDataContext();
 
             Mapper.Reset();
-            Mapper.CreateMap<IDataReader, Models.BlogSummary>()
-                .ForMember(m => m.AuthorName, opt => opt.MapFrom(r => r.GetString(r.GetOrdinal("Name"))))
-                .ForMember(m => m.AuthorAge, opt => opt.MapFrom(r => r.GetInt32(r.GetOrdinal("Age"))))
-                .ForMember(m => m.AuthorBio, opt => opt.MapFrom(r => r.GetString(r.GetOrdinal("Bio"))));
-            
-            IEnumerable<Models.BlogSummary> summaries = null;
+            Mapper.CreateMap<LinqToSql.Blog, Models.BlogSummary>();
+                //.ForMember(m => m.AuthorName, opt => opt.MapFrom(r => r.Name))
+                //.ForMember(m => m.AuthorBio, opt => opt.MapFrom(r => r.Bio))
+                //.ForMember(m => m.AuthorAge, opt => opt.MapFrom(r => r.Age))
+                //.ForMember(m => m.AuthorHometown, opt => opt.MapFrom(r => r.Hometown));
 
-            using (SqlConnection connection = new SqlConnection(@"server=.\SQLEXPRESS; DataBase=TestDB; Integrated Security=SSPI"))
-            {
-                var command = connection.CreateCommand();
-                command.CommandText = "dbo.GetAllBlogs";
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                connection.Open();
-                summaries = Mapper.Map<IDataReader, IEnumerable<Models.BlogSummary>>(command.ExecuteReader());
-            }
-                                 
-            //Mapper.CreateMap<Models.Blog, Models.BlogSummary>()
-            //    .AfterMap((src, dest) => dest.AuthorBio += "$")
-            //    .ForMember(x => x.AuthorBio, opt => 
-            //                                    {
-            //                                        opt.NullSubstitute("N/A");
-            //                                        opt.AddFormatter<BlogFormatter>();
-            //                                    });
+            //var query = db.GetAllBlogs().ToArray();            
 
-            //var summaries = Mapper.Map<Models.Blog[], Models.BlogSummary[]>(blogs)
+            var query = (from b in db.Blogs select b).ToArray();
 
-            return View(summaries.ToArray());
+            var summaries = Mapper.Map<LinqToSql.Blog[], Models.BlogSummary[]>(query);
+
+            return View(summaries);
         }
 
         public ActionResult About()
-        {
-            
+        {            
+
             return View();
         }
 
@@ -89,10 +113,10 @@ namespace MvcWithAutomapper.Controllers
             //}
             //summary.BlogId = (int)(decimal)blogIdResult;
 
-            var db = new Models.TestDBEntities();
+            var db = new ADO.TestDBEntities();
 
-            Models.Blog blog = new Models.Blog { Title = summary.Title, Description = summary.Description };
-            Models.Author author = new Models.Author { Name = summary.AuthorName, Bio = summary.AuthorBio, Hometown = summary.AuthorHometown, Age = summary.AuthorAge };
+            ADO.Blog blog = new ADO.Blog { Title = summary.Title, Description = summary.Description };
+            ADO.Author author = new ADO.Author { Name = summary.AuthorName, Bio = summary.AuthorBio, Hometown = summary.AuthorHometown, Age = summary.AuthorAge };
             db.AddToBlogs(blog);
             db.AddToAuthors(author);
             var result = db.SaveChanges();
