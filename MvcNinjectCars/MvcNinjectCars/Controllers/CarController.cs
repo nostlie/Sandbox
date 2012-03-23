cc21534760bf740addb158816638c44bfa086816
Nathan Ostlie <nostlie@gmail.com>
2012-03-23 13:17:17 -0500

    organizing

--- MvcNinjectCars/MvcNinjectCars/Controllers/CarController.cs
+++ MvcNinjectCars/MvcNinjectCars/Controllers/CarController.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using MvcNinjectCars.Attributes;
 using MvcNinjectCars.Mappers;
 using MvcNinjectCars.Models;
 using MvcNinjectCars.Models.Views;
 using MvcNinjectCars.Services;
-using System;
-using System.Linq;
 
 namespace MvcNinjectCars.Controllers
 {
