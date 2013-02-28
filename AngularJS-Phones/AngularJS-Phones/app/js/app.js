6180f871dac02870c6c6a2ddbbb3a4eccf85d2de
Nathan Ostlie <nostlie@gmail.com>
2013-02-28 09:36:21 -0600

    Tried new things to get $http.get call to work with JSON. No go.

--- AngularJS-Phones/AngularJS-Phones/app/js/app.js
+++ AngularJS-Phones/AngularJS-Phones/app/js/app.js
@@ -3,9 +3,10 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('PhoneApp', ['PhoneApp.filters', 'PhoneApp.services', 'PhoneApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/phones', { templateUrl: 'partials/phone-list.html', controller: PhoneListCtrl });
-    $routeProvider.when('/phones/:id', { templateUrl: 'partials/phone-details.html', controller: PhoneDetailCtrl });
-    $routeProvider.when('/reviews/:id', { templateUrl: 'partials/phone-reviews.html', controller: PhoneReviewCtrl });
-    $routeProvider.otherwise({ redirectTo: '/phones' });
+    config(['$routeProvider', function($routeProvider, $httpProvider) {
+        $routeProvider.when('/phones', { templateUrl: 'partials/phone-list.html', controller: PhoneListCtrl });
+        $routeProvider.when('/phones/:id', { templateUrl: 'partials/phone-details.html', controller: PhoneDetailCtrl });
+        $routeProvider.when('/reviews/:id', { templateUrl: 'partials/phone-reviews.html', controller: PhoneReviewCtrl });
+        $routeProvider.otherwise({ redirectTo: '/phones' });
+        //$httpProvider.defaults.headers.get["Content-Type"] = "application/json";
   }]);
--- AngularJS-Phones/AngularJS-Phones/app/js/controllers.js
+++ AngularJS-Phones/AngularJS-Phones/app/js/controllers.js
@@ -3,18 +3,40 @@
 /* Controllers */
 
 
-function PhoneListCtrl($scope) {    
-    $scope.phones = [
-        { id: "1", name: "Dell Streak 7", img: "../app/img/dell-streak-7.jpg" },
-        { id: "2", name: "Dell Venue", img: "../app/img/dell-venue.jpg" },
-        { id: "3", name: "Droid 2", img: "../app/img/droid-2-global-by-motorola.jpg" }
-    ];
-}
+//function PhoneListCtrl($scope, PhoneService) {    
+//    $scope.phones = PhoneService.getAllPhones();
+//    $scope.orderProp = "name";
+//}
 //PhoneListCtrl.$inject = [];
+function PhoneListCtrl($scope, $http) {
+    
+    $http.get('phones/phones.json').success(function (data) {
+        alert(data);
+    }).error(function (data, re) {
+        alert("error: " + re);
+    });
+    
+    //$.ajax({
+    //    type: "POST",
+    //    dataType: "json",
+    //    url: "phones/phones.json",
+    //    contentType: "application/json; charset=utf-8",
+    //    data: "{}"
+    //    //,
+    //    //success: function (html) {
+    //    //    alert(html);
+    //    //},
+    //    //error: function (response, re) {
+    //    //    alert("response: " + response);
+    //    //}
+        
+    //}).done(function (html) { alert("done"); }).fail(function (xhr, error) { alert("fail"); });
+    
+}
 
 
 function PhoneDetailCtrl($scope, $routeParams, PhoneService) {    
-    $scope.phone = PhoneService.getPhone($routeParams.id);    
+    $scope.phone = PhoneService.getPhone($routeParams.id);
 }
 //PhoneDetailCtrl.$inject = [];
 
